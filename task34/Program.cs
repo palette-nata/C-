240f93bf5c1c6e1696a047ac8e5586daa92a735d
Polina Sevastyanova <palette-nata@yandex.ru>
2022-10-21 22:06:20 +0700

    Задачи 34, 36, 38

---
+++ task34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+    Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(4, 100, 1000);
+Console.WriteLine();
+int sumEvenNumbers = 0;
+foreach (int el in array)
+{
+    if (el%2 == 0)
+sumEvenNumbers++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел равно {sumEvenNumbers}");
+
---
+++ task38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+double [] GetArray(int size)
+{
+double [] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+    res[i] = new Random().NextDouble();
+    Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+double MinValue (double[] array)
+{
+    double min = array[0];
+    foreach (double el in array)
+{
+    if (min > el)
+    min = el;
+}
+return min;
+}
+
+double MaxValue (double[] array)
+{
+    double max = array[0];
+    foreach (double el in array)
+{
+    if (max < el)
+    max = el;
+}
+return max;
+}
+
+double [] array = GetArray(4);
+double max = MaxValue(array);
+double min = MinValue(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равно {max-min}");
