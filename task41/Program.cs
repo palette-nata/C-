113f2ca31e611174e4ac779df2a1e241e1c62683
Polina Sevastyanova <palette-nata@yandex.ru>
2022-11-02 11:42:08 +0700

    Задачи 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите несколько чисел через пробел: ");
+string numbers = Console.ReadLine();
+
+int result = CountPositiveNumbers(numbers);
+Console.Write($"Количество чисел больше нуля равно {result}.");
+int CountPositiveNumbers(string numbers)
+{
+    string[] array = numbers.Split(' ');
+    int positivNumbers = 0;
+    foreach(string el in array)
+    {
+        int a = int.Parse(el);
+        if (a>0) 
+        positivNumbers++;
+    }
+    return positivNumbers;
+}
+
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Write("Введите значение b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = int.Parse(Console.ReadLine());
+Point2D point = IntersectionPoint(b1, k1, b2, k2);
+Console.WriteLine($"Точка пересечения двух прямых: ({point.x}, {point.y})");
+
+Point2D IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    Point2D point = new Point2D();
+    point.x =  -(b1 - b2) / (k1 - k2);
+    point.y =  k1 * point.x + b1;
+    return point;
+}
+public struct Point2D
+{
+    public double x;
+    public double y;
+}
